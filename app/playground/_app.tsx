ed52882f4cf6f7616e90f1ddc7bbb36a28beb7fb
Shakti242 <shaktipatel2421@gmail.com>
2024-08-04 19:27:48 -0400

    Link working

--- app/playground/_app.tsx
+++ app/playground/_app.tsx
@@ -1,12 +1,11 @@
-// pages/_app.tsx
-import "@uploadcare/react-widget/styles.css"; // Add this line
-import { AppProps } from 'next/app'; // Import the AppProps type from next/app
 // _app.tsx or _app.js
+import React from 'react';
+import { AppProps } from 'next/app';
 import "@uploadcare/react-widget/styles.css";
-
+import '../styles/globals.css'; // Adjust the path according to your project structure
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
